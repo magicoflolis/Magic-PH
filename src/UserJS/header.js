f402097550c09715d099536ae34c483e9a39f3fc
Magic <magicoflolis@tuta.io>
2024-03-02 19:33:25 -0800

    UserJS v3.0.0
    Signed-off-by: Magic <magicoflolis@tuta.io>

--- src/UserJS/header.js
+++ src/UserJS/header.js
@@ -2,5 +2,11 @@
 'use strict';
 let Limit_Downloads = false; // Will apply to OnlyFans only
 const debug = true;
+/**
+ * To compile this CSS `pnpm run build:Sass`
+ *
+ * Link to uncompiled Cascading Style Sheet
+ * @link https://github.com/magicoflolis/Magic-PH/tree/master/src/Common/sass
+ */
 const downloadCSS = `{{downloadCSS}}`;
 {{code}}
--- userscript/src/header.js
+++
@@ -1,8 +0,0 @@
-{jshead}
-
-'use strict';
-let Limit_Downloads = false; // Will apply to OnlyFans only
-let debug = {debugToggle};
-let mphCSS =`{mphCSS}`;
-
-{code}
